da0cf975a45b821572c2795bcf98c428d927bf0d
CELIA PIQUET <cpiquet@prismamedia.com>
2019-04-11 20:34:41 +0200

    article.name et article.category ajouté

--- services/ProductService.php
+++ services/ProductService.php
@@ -29,10 +29,18 @@ class ProductService {
     public function getAllByRoom($room_id) {
         $manager = DatabaseManager::getManager();
         $rows = $manager->getAll('
-        SELECT product.pr_id, product.limit_date, product.state, product.article_a_id, product.basket_b_id, product.room_r_id FROM product
-        JOIN room ON room.r_id = product.room_r_id AND room.r_id = ?',
-        [$room_id]
-        );
+        SELECT product.pr_id, 
+            product.limit_date, 
+            product.state, 
+            product.article_a_id, 
+            product.basket_b_id, 
+            product.room_r_id,
+            article.name,
+            article.category
+        FROM product
+        JOIN room ON room.r_id = product.room_r_id AND room.r_id = ?
+        JOIN article ON article.a_id = product.article_a_id',
+        [$room_id]);
         if (sizeof($rows)  > 0) {
             return $rows;
         }
