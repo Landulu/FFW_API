74a4b6b8c7b956880190ed97e412c8d5224fdd05
CeliaPiquet <celiapiquet@gmail.com>
2019-04-18 11:40:31 +0200

    fixe changes

--- api/users/authentication.php
+++ api/users/authentication.php
@@ -6,15 +6,15 @@ header('Content-Type: application/json');
 
 require_once __DIR__ . '/../../services/UserService.php';
 
-$userEmail = $_GET['email'].urldecode;
-$userPwd = $_GET['password'].urldecode;
+$userEmail = urldecode($_GET['email']);
+$userPwd = urldecode($_GET['password']);
 
 
 $user = UserService::getInstance()->getOneByEmail($userEmail);
 if($user) {
-    if (isset($userPwd) && isset($user->password)){
-        if( password_verify($_POST["pwd"], $resultat['password'])){
-            echo 1;
+    if (isset($userPwd) && isset($user['password'])){
+        if( password_verify($userPwd, $user['password'])){
+            return $user;
         } else {
             http_response_code(403);
         }
