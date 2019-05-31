e95c157ed2a0f8d75555f39e98b28381aac89c96
Maxime Antoine <mantoine1@myges.fr>
2019-05-31 12:02:56 +0200

    [add] get available users by skill and time interval

--- api/controllers/SkillsController.php
+++ api/controllers/SkillsController.php
@@ -5,6 +5,8 @@ include_once 'utils/routing/Request.php';
 include_once 'utils/routing/Router.php';
 
 include_once 'services/SkillService.php';
+include_once 'services/UserService.php';
+include_once 'services/AffectationService.php';
 
 
 class SkillsController {
@@ -69,6 +71,10 @@ class SkillsController {
         }
 
 
+
+        /*
+         * /skills/[skid]/users
+         */
         if ( count($urlArray) == 3
             && ctype_digit($urlArray[1])
             && $urlArray[2] == 'users'
@@ -81,6 +87,42 @@ class SkillsController {
             if($users) {
                 http_response_code(200);
                 return $users;
+            } else {
+                http_response_code(400);
+            }
+
+        }
+
+        /*
+         * /skills/[skid]/availableUsers
+         */
+        if ( count($urlArray) == 3
+            && ctype_digit($urlArray[1])
+            && $urlArray[2] == 'availableUsers'
+            && $method == 'GET') {
+
+            $offset = isset($_GET['offset']) ? intval($_GET['offset']) : 0;
+            $limit = isset($_GET['limit']) ? intval($_GET['limit']) : 20;
+            $start = new Date($_GET['start']);
+            $end = new Date($_GET['end']);
+
+
+            $users = UserService::getInstance()->getAllBySkill($urlArray[1], $offset, $limit);
+            if($users) {
+                $availableUsers = [];
+                foreach ($users as $key => $user) {
+                    $affectations = AffectationService::getInstance()->getAllByUserBetweenDates($urlArray[1],$start, $end);
+                    if (count($affectations) == 0) {
+                        array_push($availableUsers, $user);
+                    }
+                }
+                if (count($availableUsers) > 0) {
+                    http_response_code(200);
+                    return $availableUsers;
+                } else {
+                    http_response_code(204);
+                    return null;
+                }
             } else {
                 http_response_code(400);
             }
